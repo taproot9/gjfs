c33305fb2e3090fc40e9d36a1a646f4d284e8b67
Leo Nuneza <leiyu876@yahoo.com>
2018-02-14 14:10:10 +0300

    change home links to developer names, just checking the branch master if the settings is properly set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Ryan Boter</a>
+                    <a href="#">Leo Nuneza</a>
+                    <a href="#">Paylow Nuneza</a>
                 </div>
             </div>
         </div>
